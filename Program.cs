ec3c98a27a1e4aebb0606af11febbecc8861ce84
Hugo Obuchi <76051977+hugo-obuchi@users.noreply.github.com>
2021-07-16 15:15:15 +0900

    Sample code for game server.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-
 namespace AWSGameLiftServerTest
 {
     class Program
     {
+        static private GameServer server = new GameServer(); 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            server.Start();
         }
     }
 }
